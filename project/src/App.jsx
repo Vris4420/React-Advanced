41c4706542196bb51df0fef5331dc50a0e710e47
Vrishank Sharma <sharma08462@gmail.com>
2024-05-04 20:51:48 +0530

    createPortal done

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Children, useState } from "react";
+import { createPortal } from "react-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Demostration of createPortal from react-dom</h1>
+    </div>
+      <div
+        onClick={() => console.log("In the div")}
+        style={{ position: "relative", marginTop: "100px" }}
+      >
+        <h1>App Content</h1>
+        <button onClick={() => setIsOpen(true)}>Show Message</button>
+        <AlertMessage isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          Secret Message
+          <br />
+          Click To Close
+        </AlertMessage>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function AlertMessage({ children, onClose, isOpen }) {
+  if (!isOpen) return null;
+
+  return createPortal(
+    <div
+    onClick={onClose}
+      style={{
+        cursor: "pointer",
+        position: "absolute",
+        top: ".5rem",
+        left: "50%",
+        translate: "-50%",
+        background: "#777",
+        color: "white",
+        borderRadius: ".5rem",
+        padding: ".5rem",
+      }}
+    >
+      {children}
+    </div>,
+
+    document.querySelector("#alert-message")
+  );
+}
+
+export default App;
