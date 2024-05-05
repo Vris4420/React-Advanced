a3e5bde14086e07265cc019825212bfbddd6e9f0
Vrishank Sharma <sharma08462@gmail.com>
2024-05-05 19:48:14 +0530

    final commit

--- forwardRef/src/App.jsx
+++ forwardRef/src/App.jsx
@@ -1,10 +1,27 @@
+import { useRef } from "react"
+import { CustomInput } from "./CustomInput";
 
 
 function App() {
-  return(
-    <>
-    </>
+
+  const inputRef = useRef()
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(inputRef.current.value)
+  }
+
+  return (
+  <>
+  <h2>Demostration of forwardRef function</h2>
+    <form onSubmit={handleSubmit}>
+      <CustomInput ref={inputRef}/>
+      <button type="submit">Submit</button>
+    </form>
+  </>
   )
 }
+
+
 
 export default App
