b36a36ca258ed7118512461dda9d3227493a49cd
Vrishank Sharma <sharma08462@gmail.com>
2024-05-08 13:53:42 +0530

    fixes

--- forwardRef/src/App.jsx
+++ forwardRef/src/App.jsx
@@ -7,24 +7,33 @@ function App() {
   const [isDialogModalOpen, setIsDialogModalOpen] = useState(false);
 
   return (
-    <div style={{position:'relative', marginTop:'20px'}}>
+    <div style={{ position: "relative", marginTop: "20px" }}>
+      <h1>Modal Walkthrough project</h1>
       <button onClick={() => setIsCustomModalOpen(true)}>
         Show Custom Modal
-    </button>
-    <br/> <br/>
-    <button onClick={() => setIsDialogModalOpen(true)}>
-      Show Dialog Modal
-    </button>
-
-    <CustomModal isOpen={isCustomModalOpen} onClose={() => setIsCustomModalOpen(false)}>
-      <p>this is a <strong>CUSTOM</strong> modal</p>
-      <button onClick={() => setIsCustomModalOpen(false)}>Close</button>
-    </CustomModal>
-
-    <DialogModal isOpen={isDialogModalOpen} onClose={() => setIsDialogModalOpen(false)}>
-      <p>this is a <strong>DIALOG</strong> modal</p>
-      <button onClick={() => setIsDialogModalOpen(false)}>Close</button>
-    </DialogModal>
+      </button>
+      <br /> <br />
+      <button onClick={() => setIsDialogModalOpen(true)}>
+        Show Dialog Modal
+      </button>
+      <CustomModal
+        isOpen={isCustomModalOpen}
+        onClose={() => setIsCustomModalOpen(false)}
+      >
+        <p>
+          this is a <strong>CUSTOM</strong> modal
+        </p>
+        <button onClick={() => setIsCustomModalOpen(false)}>Close</button>
+      </CustomModal>
+      <DialogModal
+        isOpen={isDialogModalOpen}
+        onClose={() => setIsDialogModalOpen(false)}
+      >
+        <p>
+          this is a <strong>DIALOG</strong> modal
+        </p>
+        <button onClick={() => setIsDialogModalOpen(false)}>Close</button>
+      </DialogModal>
     </div>
   );
 }
